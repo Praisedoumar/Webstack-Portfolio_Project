08ec0df45a93c4ca7adc1b58e2c5d7b39bd3d819
Adigun Praise <adigunpraiseoluwafikayo@gmail.com>
2024-03-25 19:34:17 +0100

    websatck project

--- src/components/header/HeaderBottom.js
+++ src/components/header/HeaderBottom.js
@@ -18,7 +18,7 @@ const HeaderBottom = () => {
     });
   }, [ref, sidebar]);
   return (
-    <div className="w-full px-4 h-[36px] bg-amazon_light text-white flex items-center">
+    <div className="w-full px-4 h-[35px] bg-amazon_light text-white flex items-center">
       {/* ======================= List Items Start here ======================== */}
       <ul className="flex items-center gap-2 text-sm tracking-wide">
         <li
